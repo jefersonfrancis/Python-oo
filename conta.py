39d5b701b10251e25e94b68d6742bde5029fe7bc
Jeferson Silva <jeferson_fsilva@hotmail.com>
2019-09-25 13:45:40 -0300

    Adicionando atributos privados

--- conta.py
+++ conta.py
@@ -2,19 +2,19 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Número conta: {} \nTitular: {} \nSaldo: {} \nLimite disponível: {}".format(self.numero, self.titular, self.saldo, self.limite))
+        print("Número conta: {} \nTitular: {} \nSaldo: {} \nLimite disponível: {}".format(self.__numero, self.__titular, self.__saldo, self.__limite))
 
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
 
