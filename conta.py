afe23b754c7d0edf372c94aa2248c2f8bcadcf31
Jeferson Silva <jeferson_fsilva@hotmail.com>
2019-09-27 08:30:32 -0300

    Atualizando métodos de acesso nas classes

--- conta.py
+++ conta.py
@@ -3,7 +3,7 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         self.__numero = numero
-        self.__titular = titular
+        self.__titular = titular.title()
         self.__saldo = saldo
         self.__limite = limite
 
@@ -23,17 +23,18 @@ class Conta:
         destino.depositar(valor)
         print("Transação efetuada com sucesso!")
 
-
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
+
 
     def get_titular(self):
         return self.__titular
 
-    def get_limite(self):
+    @property
+    def limite(self):
         return self.__limite
 
-    def set_limite(self, limite):
+    @limite.setter
+    def limite(self, limite):
         self.__limite = limite
-
-
